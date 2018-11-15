b31a6b93d987c414b5fdc2c0e71e1f25cd4a0f2d
Alex Melnyk <ask.melnyk@gmail.com>
2018-11-15 16:30:26 +0200

    Assets fixes.

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,51 +1,59 @@
 import React, {Component} from 'react';
 
+import LogoImage from '../../assets/img/tpl/logo.svg';
+
 import './Header.scss';
 
 const Header = () => (
     <header
         className="header"
     >
-    	<div className="d-flex">
-	    	<div className="header-left">
-		    	<a href="#" className="nav-toggler">
-		    		<span className="icon"> icon</span>
-		    	</a>	
-		    	<a href="#" className="logo">
-		    		<img src="../../assets/img/tpl/logo.svg" alt="" width="50" />
-		    	</a>
-		    </div>	
+        <div className="d-flex">
+            <div className="header-left">
+                <a href="#" className="nav-toggler">
+                    <span className="icon"> icon</span>
+                </a>
+                <a href="#" className="logo">
+                    <img
+                        src={LogoImage}
+                        alt=""
+                        width="50"
+                    />
+                </a>
+            </div>
 
-	    	<div className="header-right">
-	    		<ul className="header-nav">
-	    			<li>
-	    				<div className="dropdown-nav">
-	    					<a className="dropdown-toggle" href="#">
-	    						<span className="arrow"><i className="icon-calendar"></i></span>
+            <div className="header-right">
+                <ul className="header-nav">
+                    <li>
+                        <div className="dropdown-nav">
+                            <a className="dropdown-toggle" href="#">
+	    						<span className="arrow">
+									<i className="icon-calendar" style={{fontSize: '20pt'}}></i>
+								</span>
 
-								<i className="icon-chat"></i>
-								<i className="icon-club"></i>
-								<i className="icon-command"></i>
-								<i className="icon-crm"></i>
-								<i className="icon-dashboard"></i>
-								<i className="icon-docs"></i>
-								<i className="icon-payment"></i>
-								<i className="icon-settings"></i>
-								<i className="icon-toggler"></i>
-								<i className="icon-tourney"></i>
-								<i className="icon-user"></i>
-	    					</a>
-							<div className="dropdown-menu">
-								
-							</div>
-						</div>
-	    					
-	    			</li>
-	    		</ul>
-		    </div>	
-		</div>
+                                {/*<i className="icon-chat"></i>*/}
+                                {/*<i className="icon-club"></i>*/}
+                                {/*<i className="icon-command"></i>*/}
+                                {/*<i className="icon-crm"></i>*/}
+                                {/*<i className="icon-dashboard"></i>*/}
+                                {/*<i className="icon-docs"></i>*/}
+                                {/*<i className="icon-payment"></i>*/}
+                                {/*<i className="icon-settings"></i>*/}
+                                {/*<i className="icon-toggler"></i>*/}
+                                {/*<i className="icon-tourney"></i>*/}
+                                {/*<i className="icon-user"></i>*/}
+                            </a>
+                            <div className="dropdown-menu">
 
-	       
+                            </div>
+                        </div>
+
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+
     </header>
 );
 
