e94fd157725d96f179409422934e41fa36912730
Alex Melnyk <ask.melnyk@gmail.com>
2018-11-20 12:56:48 +0200

    Button secondary color added.

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -3,24 +3,36 @@ import PropTypes from 'prop-types';
 
 import './Button.scss';
 
-const Button = ({icon, text, onClick}) => (
+const ButtonType = {
+    PRIMARY: 'primary',
+    SECONDARY: 'secondary',
+};
+
+const Button = ({text, icon, type, onClick}) => (
     <span
-        className="btn btn-success"
+        className={`btn btn-${type}`}
         onClick={() => onClick && onClick()}
     >
-            {
-                icon && (
-                    <i className={`ft-${icon}`}/>
-                )
-            }
+        {
+            icon && (
+                <i className={`ft-${icon}`}/>
+            )
+        }
         <span>{text}</span>
     </span>
 );
 
 Button.propTypes = {
-    icon: PropTypes.string,
     text: PropTypes.string.isRequired,
+    icon: PropTypes.string,
+    type: PropTypes.oneOf(Object.values(ButtonType)),
     onClick: PropTypes.func
 };
 
-export {Button};
+Button.defaultProps = {
+    type: ButtonType.PRIMARY
+};
+
+Button.ButtonType = ButtonType;
+
+export {Button, ButtonType};
